12cb40f6e2b3940dec78b6d42ab33eb4f197c663
JAEWEONLEE <34277606+jaeweon@users.noreply.github.com>
2023-04-23 22:53:26 +0900

    Add files via upload
    jsp spring 넘어가기전 다시 java 기초 다지기

---
+++ Study01.java
@@ -0,0 +1,11 @@
+public class Study01 {
+    public static void main(String[] args) {
+
+
+        System.out.println("로그 찍습니다.");
+
+        // 변수를 선언
+        int i = 0;
+        System.out.println("i " + i);
+    }
+}
